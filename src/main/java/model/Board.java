82a6e890a67ed77c6d85dadbfc7f87e01aa21a97
sebmichalowski <seb.michalowski@gmail.com>
2017-06-13 10:35:42 +0200

    Board init()

--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -1,8 +1,23 @@
 package model;
 
 public class Board {
-    public static Cell cells[][];
+    private Cell cells[][];
+
+    public void setCells(Cell[][] cells) {
+        this.cells = cells;
+    }
 
     void init() {
+        int boardSize = 3;
+        cells = new Cell[boardSize][boardSize];
+        for (int i=0; i<boardSize;i++) {
+            for (int j=0; j<boardSize; j++) {
+                cells[i][j] = new Cell(i+1, j+1);
+            }
+        }
+    }
+
+    public Cell[][] getCells() {
+        return cells;
     }
 }
--- src/test/java/model/BoardTest.java
+++ src/test/java/model/BoardTest.java
@@ -18,15 +18,15 @@ class BoardTest {
 
     @BeforeEach
     void setUp() {
-        cell = mock(Cell.class);
+        board = new Board();
 
-        int columns = 3;
-        int rows = 3;
-        Cell array[][] = new Cell[columns][rows];
-        for (int col = 0; col < columns; col++) {
-            for (int row = 0; row < rows; row++) {
-                array[col][row] = cell;
-            }
+//        int columns = 3;
+//        int rows = 3;
+//        Cell array[][] = new Cell[columns][rows];
+//        for (int col = 0; col < columns; col++) {
+//            for (int row = 0; row < rows; row++) {
+//                array[col][row] = cell;
+//            }
         }
 
 //        for (Cell col []: array) {
@@ -36,57 +36,60 @@ class BoardTest {
 //
 //            }
 //        }
-        board = new Board();
-    }
+
 
     @Test
     void testGetCells_returnsCells(){
         Cell[][] cells = new Cell[3][3];
         board.setCells(cells);
-        assertEquals(cells, board.getCells);
+        assertEquals(cells, board.getCells());
     }
 
     @Test
     void testInit_AfterCall_CellsHaveThreeRows(){
         board.init();
-        assertEquals(3, board.getCells.length);
+        assertEquals(3, board.getCells().length);
     }
 
     @Test
     void testInit_AfterCall_CellsHaveThreeCols(){
         board.init();
-        assertAll(assertEquals(3, board.getCells[0].length),
-                  assertEquals(3, board.getCells[1].length),
-                  assertEquals(3, board.getCells[2].length));
+        assertEquals(3, board.getCells()[0].length);
+        assertEquals(3, board.getCells()[1].length);
+        assertEquals(3, board.getCells()[2].length);
     }
-
+//
     @Test
     void testInit_AfterCall_AllCellsAreEmpty(){
         board.init();
-        assertTrue(board.getCells.stream().allMatch(x -> x.getContent().equals(Seed.EMPTY)));
+        assertTrue(Arrays.stream(board.getCells()[0]).allMatch(x -> x.getContent().equals(Seed.EMPTY)));
+        assertTrue(Arrays.stream(board.getCells()[1]).allMatch(x -> x.getContent().equals(Seed.EMPTY)));
+        assertTrue(Arrays.stream(board.getCells()[2]).allMatch(x -> x.getContent().equals(Seed.EMPTY)));
     }
 
     @Test
     void testInit_AfterCall_AllCellsHaveCorrectColAndRowValue() {
-        for (int i=0; i<3; i++) {
-            for(int j=0; j<3; j++) {
-                assertEquals(j+1, board.getCells[i][j].getCol());
-                assertEquals(i+1, board.getCells[i][j].getRow());
+        board.init();
+        int boardSize = 3;
+        for (int i=0; i<boardSize; i++) {
+            for(int j=0; j<boardSize; j++) {
+                assertEquals(j+1, board.getCells()[i][j].getCol());
+                assertEquals(i+1, board.getCells()[i][j].getRow());
             }
         }
     }
+
     @Test // winning conditions needed
     void test_isDraw(){
         for (int col = 0; col < columns; col++) {
             for (int row = 0; row < rows; row++) {
                 Cell expectedCell = array[col][row];
                 Boolean isExpected = (!expectedCell.getContent().equals(Seed.EMPTY)); // if not EMPTY = true
-                Cell actualCell = Board.cells[col][row];
+                Cell actualCell = board.getCells()[col][row];
                 Boolean isActual = (!actualCell.getContent().equals(Seed.EMPTY)); // if not EMPTY = true
 
                 assertEquals(isExpected, isActual);
             }
         }
     }
-
 }
