48d0fdb0e998925775a5c015541dcbeb824b641c
totine <joanna.gargas@gmail.com>
2017-06-14 12:05:40 +0200

    Add app and game controller classes

---
+++ src/main/java/App.java
@@ -0,0 +1,36 @@
+/**
+ * Created by joanna on 14.06.17.
+ */
+public class App {
+    GameController gameController;
+    boolean isAppActive;
+
+    private App() {
+        gameController = new GameController();
+        isAppActive = true;
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.gameInit();
+
+    }
+
+    private void askToContinue() {
+        isAppActive = false;
+    }
+
+    private void gameInit() {
+        while (isAppActive) {
+            gameController.initGame();
+            gameController.chooseGameMode();
+            while (gameController.getIsGameEnd()) {
+                gameController.showCurrentPlayer();
+                gameController.getInputFromPlayer();
+                gameController.showGameStatus();
+            }
+            gameController.showGameResults();
+            askToContinue();
+        }
+    }
+}
---
+++ src/main/java/GameController.java
@@ -0,0 +1,29 @@
+import model.Game;
+
+public class GameController {
+    Game game;
+
+    public void initGame() {
+        game = new Game();
+
+    }
+
+    public void chooseGameMode() {
+    }
+
+    public boolean getIsGameEnd() {
+    return false;
+    }
+
+    public void showCurrentPlayer() {
+    }
+
+    public void getInputFromPlayer() {
+    }
+
+    public void showGameStatus() {
+    }
+
+    public void showGameResults() {
+    }
+}
